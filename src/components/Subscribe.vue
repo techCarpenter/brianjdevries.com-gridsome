5103ab854eab7f2a5063a426ecf5ed76a42387f9
Brian DeVries <bdevries95@gmail.com>
2020-08-24 22:15:57 -0400

    Added subscribe button to default layout

---
+++ src/components/Subscribe.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="subscribe">
+    <p><strong>Get updates every Monday</strong></p>
+    <a class="button" :href="$static.json.newsletter">Subscribe!</a>
+  </div>
+</template>
+
+<static-query>
+query {
+  json(id: 0) {
+    newsletter
+  }
+}
+</static-query>
+
+<style scoped>
+.subscribe {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto 3em;
+}
+.button {
+  padding: 10px;
+  color: #eee;
+  background-color: var(--cinnabar);
+  border-radius: 6px;
+}
+.button:hover,
+.button:focus {
+  text-decoration: none;
+  filter: brightness(1.2);
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,6 +4,8 @@
     <div class="layout">
       <slot />
     </div>
+    <Aside />
+    <Subscribe />
     <Footer />
   </div>
 </template>
@@ -11,11 +13,15 @@
 <script>
 import Header from "./../components/Header";
 import Footer from "./../components/Footer";
+import Aside from "./../components/Aside";
+import Subscribe from "./../components/Subscribe";
 
 export default {
   components: {
     Header,
     Footer,
+    Aside,
+    Subscribe,
   },
 };
 </script>
@@ -27,7 +33,7 @@ export default {
   padding: 0 10px;
 }
 
-@media (min-width: 760px) {
+@media (min-width: 620px) {
   .layout {
     padding: 0;
   }
